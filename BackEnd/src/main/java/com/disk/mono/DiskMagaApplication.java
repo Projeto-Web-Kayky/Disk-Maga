fa009673ab01e8fdfad8e366744b0b973369580b
KaykySiq <josekayky@hotmail.com>
2025-10-01 15:20:10 -0300

    Build: Configura o BackEnd.

---
+++ BackEnd/src/main/java/com/disk/mono/DiskMagaApplication.java
@@ -0,0 +1,13 @@
+package com.disk.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskMagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiskMagaApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/disk/mono/DiskMagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disk.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskMagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
