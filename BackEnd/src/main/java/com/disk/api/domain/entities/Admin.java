67f95e23444adddcb63bfa2e19200f91c2b262fb
KaykySiq <josekayky@hotmail.com>
2025-10-01 15:46:53 -0300

    feat: Add entity Admin.

--- BackEnd/src/main/java/com/disk/api/domain/entities/Admin.java
+++ BackEnd/src/main/java/com/disk/api/domain/entities/Admin.java
@@ -1,5 +1,28 @@
 package com.disk.api.domain.entities;
 
-public class Admin {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "admin")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+    
+    private String name;
+    private String email;
+    private String password;
 }
