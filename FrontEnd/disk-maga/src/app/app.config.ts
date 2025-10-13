255d0b66b7f26f7d31605120154e44a107ebc0bf
KaykySiq <josekayky@hotmail.com>
2025-10-13 18:34:52 -0300

    feat: Torna a tela de login dinamica com autenticação.

--- FrontEnd/disk-maga/src/app/app.config.ts
+++ FrontEnd/disk-maga/src/app/app.config.ts
@@ -1,17 +1,43 @@
-import { provideEventPlugins } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AuthInterceptor } from './auth.interceptor';
+
+export function tokenGetter() {
+  return localStorage.getItem('auth_token');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideAnimations(),
-        provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-        provideEventPlugins(),
-        provideEventPlugins()
-    ]
+    provideEventPlugins(),
+    provideEventPlugins(),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8080'],
+          disallowedRoutes: ['http://localhost:8080/auth/login'],
+        },
+      })
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 };
