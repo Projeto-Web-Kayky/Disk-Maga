72c4182a3f58cebc4f556fe062b73360e9211cc1
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2025-10-13 21:18:34 -0300

    feat(componente): nav-bar finalizada

--- FrontEnd/disk-maga/src/app/app.routes.ts
+++ FrontEnd/disk-maga/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AntiAuthGuard } from './anti-auth.guard';
+import { NavBar } from './components/nav-bar/nav-bar.component';
 
 export const publicRoutes = ['/login'];
 
@@ -9,5 +10,14 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent,
         canActivate: [AntiAuthGuard]
+    },
+    {
+        path: 'navbar',
+        component: NavBar
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
     }
 ];
--- FrontEnd/disk-maga/src/app/components/nav-bar/nav-bar.component.ts
+++ FrontEnd/disk-maga/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class NavBar {
 
+  constructor(private router: Router){ }
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
